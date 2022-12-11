db02208aa3c6afec34e41a414391c110bf2b420e
AdithyaOthayoth <118025644+AdithyaOthayoth@users.noreply.github.com>
2022-12-11 02:04:08 +0000

    Updated code for creating second graph

--- Population.py
+++ Population.py
@@ -10,24 +10,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #A function to plot the bar graph  
-def barGraph():
+def barGraph(df,columns, xlabel, ylabel, title):
     """
     The function bargraph() is used to plot a bar graph representing the percentage of people
-    living in the urban area in each selected country, during the period of 1965 to 2015.
+    living in the urban area in each selected country,and the effect of urbanisation in the agricultural 
+    land during the period of 1965 to 2015.
     """    
-    labels = df_population["Country Name"]
-    years=['1965','1975','1985','1995','2005','2015']
+    labels = df["Country Name"]
     x = np.arange(len(labels))
     w = 0.12
     plt.figure()
     fig, ax = plt.subplots()
     i=0
-    for y in years:
-        ax.bar(x+(i*w),df_population[y],width=w,label=y,edgecolor='k', align='center')
+    for y in columns:
+        ax.bar(x+(i*w),df[y],width=w,label=y,edgecolor='k', align='center')
         i=i+1   
-    ax.set_ylabel('Population Percentage')
-    ax.set_xlabel('Country')
-    ax.set_title('Urban population (% of total population)')
+    ax.set_ylabel(ylabel)
+    ax.set_xlabel(xlabel)
+    ax.set_title(title)
     ax.set_xticks(x, labels)
     ax.legend()    
     fig.tight_layout()  
@@ -37,13 +37,31 @@ def barGraph():
 dataloc="Dataset.csv"
 Data = pd.read_csv("Dataset.csv",sep='\t',skiprows=4,engine='python')
 df_Data = pd.DataFrame(Data)
-
-#Choosing indicator and updating dataframe according to that
+#Choosing indicator (Urban population (% of total population)) and updating dataframe according to that
 urbanPopulation = df_Data.loc[df_Data['Indicator Name'].isin(["Urban population (% of total population)"])]
 df_urbanPopulation = urbanPopulation.dropna()
 df_urbanPopulationSelected = df_urbanPopulation.loc[df_urbanPopulation['Country Code'].isin(['ABW','BRA','IND','ARG','HRV','CHN'])]
 df_population = df_urbanPopulationSelected.loc[:, ['Country Name','1965', '1975', '1985', '1995', '2005', '2015']].reset_index(drop=True)
 
+#Choosing indicator (Agricultural land (% of land area)) and updating dataframe according to that
+df_agriculturalLand = df_Data.loc[df_Data['Indicator Name'].isin(["Agricultural land (% of land area)"])]
+df_agriculturalLandSelected = df_agriculturalLand.loc[df_agriculturalLand['Country Code'].isin(['ABW','BRA','IND','ARG','HRV','CHN'])]
+df_agriculturalLand = df_agriculturalLandSelected.loc[:, ['Country Name','1965', '1975', '1985', '1995', '2005', '2015']].reset_index(drop=True)
+print(df_agriculturalLand)
+years=['1965','1975','1985','1995','2005','2015']
+
+
 #Calling functions to plot the grpahs
-barGraph()
+barGraph(df_population,years,'Country','Population Percentage','Urban population (% of total population)')
+barGraph(df_agriculturalLand,years,'Country','Agricultural land Area Percentage','Agricultural land (% of land area)')
+
+
+
+
+
+
+
+
+
+
 
